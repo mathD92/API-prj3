ad3d314d19d0de0d8a38eadd3c869096a82f4dd3
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-02-12 07:35:13 +0100

    Define the MainFragment, like the home page

--- app/src/main/java/com/feetconnect/MainActivity.kt
+++ app/src/main/java/com/feetconnect/MainActivity.kt
@@ -2,13 +2,33 @@ package com.feetconnect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+}
+
+// Create a method only AppCompactActivity and after you use in the project when we call
+// the AppCompactActivity
+fun AppCompatActivity.replaceFragment(fragment: Fragment){
+    // Created variable for recover the methods
+    val fragmentManager = supportFragmentManager
+    // Created variable for realized the transaction with fragment and application method
+    // beginTransaction()
+    val transaction = fragmentManager.beginTransaction()
+    // Here, we target the fragment element which is in the activity_main.xml
+    // and fragment replace as argument
+    transaction.replace(R.id.fragment,fragment)
+    // This method permit to back in the fragment prevent with the button return of the phone
+    transaction.addToBackStack(null)
+    // Commit for execute the method
+    transaction.commit()
 }
 
 
