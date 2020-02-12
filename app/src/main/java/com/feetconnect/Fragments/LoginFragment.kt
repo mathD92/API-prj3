16e56f8dced0dc329bc0870f01e0808ddcd789b8
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-02-12 08:57:14 +0100

    Integration of function for the buttons in the Login Fragment

--- app/src/main/java/com/feetconnect/Fragments/LoginFragment.kt
+++ app/src/main/java/com/feetconnect/Fragments/LoginFragment.kt
@@ -6,8 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 import com.feetconnect.R
+import com.feetconnect.replaceFragment
 
 /**
  * A simple [Fragment] subclass.
@@ -20,6 +25,22 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val act = activity as AppCompatActivity
+        val btn_connect: Button = view.findViewById(R.id.btn_connect)
+        val btn_clear: Button = view.findViewById(R.id.btn_clear)
+        val edit_email: EditText = view.findViewById(R.id.edit_email)
+        val edit_password: EditText = view.findViewById(R.id.edit_password)
+
+        btn_connect.setOnClickListener() {
+            Toast.makeText(context, edit_email.getText(), Toast.LENGTH_SHORT).show()
+            act.replaceFragment(MainFragment())
+        }
+
+        btn_clear.setOnClickListener(){
+            edit_email.text.clear()
+            edit_password.text.clear()
+        }
 
         return view
     }
