c71486b34f245464a44fc4527e6ecfcf625752c8
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-02-13 14:42:08 +0100

    Add the edition textview in the xml with the values sensors

--- app/src/main/java/com/feetconnect/Fragments/SensorFragment.kt
+++ app/src/main/java/com/feetconnect/Fragments/SensorFragment.kt
@@ -11,8 +11,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 import com.feetconnect.R
+import kotlinx.android.synthetic.main.fragment_sensor.*
 
 /**
  * A simple [Fragment] subclass.
@@ -34,6 +36,12 @@ class SensorFragment : Fragment(), SensorEventListener {
         sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
         sensor1 = sensorManager!!.getDefaultSensor(Sensor.TYPE_ORIENTATION)
 
+        val title_sensor_acceleration: TextView = view.findViewById(R.id.title_sensor_acceleration)
+        val title_sensor_orientation: TextView = view.findViewById(R.id.title_sensor_orientation)
+
+        title_sensor_acceleration.setText("Sensor Acceleration Linear")
+        title_sensor_orientation.setText("Sensor Orientation")
+
         return view
     }
 
@@ -49,7 +57,16 @@ class SensorFragment : Fragment(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-
+        if (event!!.sensor.type === Sensor.TYPE_LINEAR_ACCELERATION ){
+            sensor_acceleration_x.setText("X : " + event!!.values[0].toString())
+            sensor_acceleration_y.setText("Y : " + event.values[1].toString())
+            sensor_acceleration_z.setText("Z : " + event.values[2].toString())
+        }
+        if (event!!.sensor.type === Sensor.TYPE_ORIENTATION){
+            sensor_orientation_x.setText("X : " + event!!.values[0].toString())
+            sensor_orientation_y.setText("Y : " + event.values[1].toString())
+            sensor_orientation_z.setText("Z : " + event.values[2].toString())
+        }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
