8a65a736b9ba1dbedf0adaca98e828663e76c163
ConteAlexandre <pro.alexandre.conte@gmail.com>
2020-02-13 14:03:44 +0100

    Add the fragment sensor in the activity_main.xml and setup the sensors in the class SensorFragments

--- app/src/main/java/com/feetconnect/Fragments/SensorFragment.kt
+++ app/src/main/java/com/feetconnect/Fragments/SensorFragment.kt
@@ -1,6 +1,11 @@
 package com.feetconnect.Fragments
 
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,7 +17,11 @@ import com.feetconnect.R
 /**
  * A simple [Fragment] subclass.
  */
-class SensorFragment : Fragment() {
+class SensorFragment : Fragment(), SensorEventListener {
+
+    var sensor: Sensor? = null
+    var sensor1: Sensor? = null
+    var sensorManager: SensorManager? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +29,31 @@ class SensorFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_sensor, container, false)
-        
+
+        sensorManager = getActivity()?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+        sensor1 = sensorManager!!.getDefaultSensor(Sensor.TYPE_ORIENTATION)
+
         return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager!!.registerListener(this, sensor1, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager!!.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
     }
 
 
